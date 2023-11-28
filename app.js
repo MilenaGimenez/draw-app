b381b982a14fd894002da9dcbd5d957d3492fe4f
Milena Gimenez <milenanicolegimenez@gmail.com>
2023-11-28 14:31:52 -0300

    añadiendo boton de descarga de la imagen generada

--- app.js
+++ app.js
@@ -5,15 +5,31 @@ const context = mainCanvas.getContext("2d");
 //Colores
 const red = document.getElementById("red"); 
 const orange = document.getElementById("orange"); 
+const yellow = document.getElementById("yellow"); 
 
 //Agarrar coordenadas del mouse y dibujar cuando este el click pulsado
 let initialX;
 let initialY;
 
+
 let colorPincel = context.strokeStyle = "#000";
 let colorRojo = "#ff0000";
 let colorNaranja = "#ffa500";
+let colorAmarillo = "#ffff00";
 
+const listaDeColores = [colorRojo, colorNaranja, colorAmarillo]
+
+// const cambiarColor = (color) => {
+//     for (let index = 0; index < listaDeColores.length; index++) {
+//         // const element = array[index];
+//         console.log("prueba")
+    
+//         color.addEventListener("click", () => {
+//             colorPincel = context.strokeStyle = listaDeColores[index];
+//             console.log("hice click en un color")        
+//         }); 
+//     }
+// };
 
 red.addEventListener("click", () => {
     colorPincel = context.strokeStyle = colorRojo;
@@ -22,6 +38,11 @@ red.addEventListener("click", () => {
 
 orange.addEventListener("click", () => {
     colorPincel = context.strokeStyle = colorNaranja;
+    console.log("hice click en un color")        
+});   
+
+yellow.addEventListener("click", () => {
+    colorPincel = context.strokeStyle = colorAmarillo;
     console.log("hice click en un color")        
 });   
 
@@ -64,3 +85,15 @@ const mouseUp = () => {
 
 mainCanvas.addEventListener('mousedown', mouseDown);
 mainCanvas.addEventListener('mouseup', mouseUp); //Asociando función de dejar de dibujar
+
+
+//Descargar imagen:
+const descargarImg = () => {
+    // let imagen = mainCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+    // window.location.href = imagen;
+
+    flor = document.createElement('a');
+    flor.download = "Sin título";
+    flor.href = mainCanvas.toDataURL("image/png");
+    flor.click();
+};
